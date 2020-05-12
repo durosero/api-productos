c0cb4e67cd2c0777522c0ab61d569380db81a898
Duvan Rosero <duvanrosero1995@gmail.com>
2020-05-12 18:37:45 -0500

    añadimos las CORS para permitir todas peticiones

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php
 require __DIR__ . '/library/AltoRouter.php';
+//=====================================
+//   cors origin
+//=====================================
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+$method = $_SERVER['REQUEST_METHOD'];
+if($method == "OPTIONS") {
+    die();
+}
 
 $router = new AltoRouter();
 $router->setBasePath('/productos');
